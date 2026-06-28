db1f6fb516f92c9de9ba1b53150027bbf84df805
CVSI <contact@CVSI.nc>
2026-06-28 15:07:11 +1100

    fix: correction des tests des composants

--- src/app/produit-card/produit-card.spec.ts
+++ src/app/produit-card/produit-card.spec.ts
@@ -13,6 +13,13 @@ describe('ProduitCard', () => {
 
     fixture = TestBed.createComponent(ProduitCard);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('produit', {
+      id: 1,
+      nom: 'Test',
+      prix: 10,
+      stock: 5,
+      categorie: 'Test',
+    });
     await fixture.whenStable();
   });
 
